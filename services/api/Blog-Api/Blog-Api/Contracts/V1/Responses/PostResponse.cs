0806a297348225f49922768cba25441ff81abc92
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-07-09 02:22:36 +0200

    Update PostResponse do provide documentation and examples.

--- services/api/Blog-Api/Blog-Api/Contracts/V1/Responses/PostResponse.cs
+++ services/api/Blog-Api/Blog-Api/Contracts/V1/Responses/PostResponse.cs
@@ -1,3 +1,41 @@
 namespace BlogApi.Contracts.V1.Responses;
 
-public record PostResponse(Guid Id, string Title, string Slug, string Body);
+public record PostResponse
+{
+    /// <summary>
+    /// Guid of the post.
+    /// </summary>
+    public Guid Id { get; }
+
+    /// <summary>
+    /// Title of the post.
+    /// </summary>
+    public string Title { get; }
+
+    /// <summary>
+    /// The slug of this post, derived from the title
+    /// </summary>
+    /// <example>this-is-an-article-title</example>
+    public string Slug { get; }
+
+    /// <summary>
+    /// Content of the post. In a Markdown format.
+    /// </summary>
+    public string Body { get; }
+
+    public PostResponse(Guid Id, string Title, string Slug, string Body)
+    {
+        this.Id = Id;
+        this.Title = Title;
+        this.Slug = Slug;
+        this.Body = Body;
+    }
+
+    public void Deconstruct(out Guid id, out string title, out string slug, out string body)
+    {
+        id = Id;
+        title = Title;
+        slug = Slug;
+        body = Body;
+    }
+}
