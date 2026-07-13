ea4f0eeedc34e3076999f73bb2c6e8b00ec4563b
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-07-13 20:37:46 +0200

    Implement TagsService

--- services/api/Blog-Api/Blog-Api/Services/Tags/ITagsService.cs
+++ services/api/Blog-Api/Blog-Api/Services/Tags/ITagsService.cs
@@ -1,5 +1,17 @@
+using BlogApi.Domain;
+
 namespace BlogApi.Services.Tags;
 
 public interface ITagsService
 {
+    public Task<Tag?> GetTag(Guid id);
+    public Task<Tag?> GetTag(string slug);
+    public Task<List<Tag>> GetAllTags();
+    public Task<List<Tag>> GetAllTags(IReadOnlyCollection<Guid> ids);
+    public Task<List<Tag>> GetAllTags(IReadOnlyCollection<string> slugs);
+    public Task<List<Tag>> GetAllTags(IReadOnlyCollection<Guid> ids, IReadOnlyCollection<string> slugs);
+
+    public Task<bool> AddTag(Tag tag);
+    public Task<bool> DeleteTag(Tag tag);
+    public Task<bool> UpdateTag(Tag tag);
 }
--- services/api/Blog-Api/Blog-Api/Services/Tags/TagsService.cs
+++ services/api/Blog-Api/Blog-Api/Services/Tags/TagsService.cs
@@ -1,3 +1,4 @@
+using BlogApi.Domain;
 using BlogApi.Repositories.Tags;
 
 namespace BlogApi.Services.Tags;
@@ -9,5 +10,50 @@ public class TagsService : ITagsService
     public TagsService(ITagsRepository tagsRepository)
     {
         _tagsRepository = tagsRepository;
+    }
+
+    public async Task<Tag?> GetTag(Guid id)
+    {
+        return await _tagsRepository.GetTagById(id);
+    }
+
+    public async Task<Tag?> GetTag(string slug)
+    {
+        return await _tagsRepository.GetTagBySlug(slug);
+    }
+
+    public async Task<List<Tag>> GetAllTags()
+    {
+        return await _tagsRepository.GetAllTags();
+    }
+
+    public async Task<List<Tag>> GetAllTags(IReadOnlyCollection<Guid> ids)
+    {
+        return await _tagsRepository.GetAllTagsById(ids);
+    }
+
+    public async Task<List<Tag>> GetAllTags(IReadOnlyCollection<string> slugs)
+    {
+        return await _tagsRepository.GetAllTagsBySlug(slugs);
+    }
+
+    public async Task<List<Tag>> GetAllTags(IReadOnlyCollection<Guid> ids, IReadOnlyCollection<string> slugs)
+    {
+        return await _tagsRepository.GetAllTagsByIdOrSlug(ids, slugs);
+    }
+
+    public async Task<bool> AddTag(Tag tag)
+    {
+        return await _tagsRepository.AddTag(tag);
+    }
+
+    public async Task<bool> DeleteTag(Tag tag)
+    {
+        return await _tagsRepository.DeleteTag(tag);
+    }
+
+    public async Task<bool> UpdateTag(Tag tag)
+    {
+        return await _tagsRepository.UpdateTag(tag);
     }
 }
