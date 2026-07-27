287cd3bee19fb82e19d09d73b72551f684681fed
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-07-28 01:28:28 +0200

    Fix PostsRepository not being registered as service.

--- services/api/Blog-Api/Blog-Api/Installers/RepositoriesInstaller.cs
+++ services/api/Blog-Api/Blog-Api/Installers/RepositoriesInstaller.cs
@@ -1,3 +1,4 @@
+using BlogApi.Repositories.Posts;
 using BlogApi.Repositories.Tags;
 
 namespace BlogApi.Installers;
@@ -7,6 +8,8 @@ public static class RepositoriesInstaller
     public static IServiceCollection InstallRepositories(this IServiceCollection services)
     {
         services.AddScoped<ITagsRepository, TagsRepository>();
+        services.AddScoped<IPostsRepository, PostsRepository>();
+
         return services;
     }
 }
