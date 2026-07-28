2a263950cb7a051779b4796489e44025888b5011
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-07-29 00:59:03 +0200

    Remove required keywords for properties in UpdatePostRequest dto since they can be ommited.

--- services/api/Blog-Api/Blog-Api.Contracts/V1/Requests/UpdatePostRequest.cs
+++ services/api/Blog-Api/Blog-Api.Contracts/V1/Requests/UpdatePostRequest.cs
@@ -2,7 +2,7 @@ namespace BlogApi.Contracts.V1.Requests;
 
 public record UpdatePostRequest
 {
-    public required string? Title { get; init; }
-    public required string? Body { get; init; }
-    public required IReadOnlyCollection<string>? Tags { get; init; }
+    public string? Title { get; init; }
+    public string? Body { get; init; }
+    public IReadOnlyCollection<string>? Tags { get; init; }
 }
