bc6bfdf68c7db4968afab81b0eda85577bfbb9a0
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-07-31 20:19:56 +0200

    Limit comments length to 3000 characters

--- services/api/Blog-Api/Blog-Api/Domain/Comment.cs
+++ services/api/Blog-Api/Blog-Api/Domain/Comment.cs
@@ -5,9 +5,12 @@ namespace BlogApi.Domain;
 
 public class Comment : BaseEntity
 {
+    public const int BodyMaxLength = 3000;
+
     [MaxLength(256)]
     public required string Username { get; set; }
 
+    [MaxLength(BodyMaxLength)]
     public required string Body { get; set; }
 
     public required DateTimeOffset CreatedAt { get; set; }
---
+++ services/api/Blog-Api/Blog-Api/Migrations/20260731181743_LimitCommentLength.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class LimitCommentLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "body",
+                table: "comments",
+                type: "character varying(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "body",
+                table: "comments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(3000)",
+                oldMaxLength: 3000);
+        }
+    }
+}
