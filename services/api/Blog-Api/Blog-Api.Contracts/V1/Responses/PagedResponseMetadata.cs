2cd59225586b5d22aa9124e4977c6d2544e3fef7
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-08-06 14:34:32 +0200

    Add doc to the PagedResponseMetadata

--- services/api/Blog-Api/Blog-Api.Contracts/V1/Responses/PagedResponseMetadata.cs
+++ services/api/Blog-Api/Blog-Api.Contracts/V1/Responses/PagedResponseMetadata.cs
@@ -6,12 +6,24 @@ public record PagedResponseMetadata
 {
     public long Count { get; init; }
 
+    /// <summary>
+    /// Current page
+    /// </summary>
+    /// <example>1</example>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? PageNumber { get; init; }
 
+    /// <summary>
+    /// Size of the page
+    /// </summary>
+    /// <example>20</example>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public int? PageSize { get; init; }
 
+    /// <summary>
+    /// Sort applied to request
+    /// </summary>
+    /// <example>id</example>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? SortBy { get; init; }
 }
