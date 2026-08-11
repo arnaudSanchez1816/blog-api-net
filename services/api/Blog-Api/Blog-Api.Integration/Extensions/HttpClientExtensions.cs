dcc7b859df4b72058266d461e5eb18acc0c5cf95
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-08-11 15:38:13 +0200

    Add more extensions methods for http client messages with bearer token

--- services/api/Blog-Api/Blog-Api.Integration/Extensions/HttpClientExtensions.cs
+++ services/api/Blog-Api/Blog-Api.Integration/Extensions/HttpClientExtensions.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace BlogApi.Integration.Extensions;
@@ -11,11 +12,25 @@ public static class HttpClientExtensions
         public Task<HttpResponseMessage> GetWithBearerAsync([StringSyntax("Uri")] string requestUri,
             string? bearerToken, CancellationToken ct = default)
         {
+            return client.SendWithBearerAsync(HttpMethod.Get, new Uri(requestUri), bearerToken, ct);
+        }
+
+        public Task<HttpResponseMessage> GetWithBearerAsync(Uri requestUri,
+            string? bearerToken, CancellationToken ct = default)
+        {
             return client.SendWithBearerAsync(HttpMethod.Get, requestUri, bearerToken, ct);
         }
 
+        public Task<HttpResponseMessage> PostWithBearerAsJsonAsync<TValue>([StringSyntax("Uri")] string requestUri,
+            TValue value,
+            string? bearerToken,
+            CancellationToken ct = default)
+        {
+            return client.SendWithBearerAsync(HttpMethod.Post, requestUri, value, bearerToken, ct);
+        }
+
         private Task<HttpResponseMessage> SendWithBearerAsync(HttpMethod method,
-            string requestUri, string? bearerToken, CancellationToken ct = default)
+            Uri requestUri, string? bearerToken, CancellationToken ct = default)
         {
             HttpRequestMessage request = new HttpRequestMessage(method, requestUri);
             if (bearerToken is not null)
@@ -23,6 +38,22 @@ public static class HttpClientExtensions
                 request.Headers.Authorization =
                     new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, bearerToken);
             }
+
+            return client.SendAsync(request, ct);
+        }
+
+        private Task<HttpResponseMessage> SendWithBearerAsync<TValue>(HttpMethod method,
+            string requestUri, TValue value, string? bearerToken, CancellationToken ct = default)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(method, requestUri);
+            if (bearerToken is not null)
+            {
+                request.Headers.Authorization =
+                    new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, bearerToken);
+            }
+
+            JsonContent content = JsonContent.Create(value);
+            request.Content = content;
 
             return client.SendAsync(request, ct);
         }
