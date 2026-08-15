073e3b54efe593ac51c800b45b05824e749f2811
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2026-08-15 17:39:45 +0200

    Add migration for the published post index

---
+++ services/api/Blog-Api/Blog-Api/Migrations/20260815153911_AddPublishedPostIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublishedPostIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_posts_published_at",
+                table: "posts",
+                column: "published_at",
+                filter: "published_at IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_posts_published_at",
+                table: "posts");
+        }
+    }
+}
